495f662e8373fc38f1e03555d41d60a0f30119bc
Xavier Dunkley <xavierxv18@gmail.com>
2022-01-29 18:10:55 -0500

    commit tree 1fdd5d74560a1bf099f090e2cd99c174318e450a

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1=2;
+let num2=31;
+let num3=31;
+let num4=9;
+
+let multiply = num1*num2;
+multiply;
+
+let random=Math.floor(Math.random()*10)+1;
+random;
+
+let mod=num3%num4;
+mod;
+
+let max=Math.max(2,3,6,20,16);
+max;
